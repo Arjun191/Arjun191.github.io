814515fa9cf4479480a877a1c83f98025b8ba3d5
Arjun191 <arjun.pm2000@gmail.com>
2024-09-21 12:52:35 -0400

    profile pic updated2

--- js/profile-card.js
+++ js/profile-card.js
@@ -9,7 +9,7 @@ window.addEventListener("DOMContentLoaded", async function () {
         // Simulate fetching user data (manually added since LinkedIn's API is private)
         const profile = {
             profile_image: "https://media.licdn.com/dms/image/C5603AQGd8LiPAzF5Kg/profile-displayphoto-shrink_800_800/0/1623864015946?e=1700092800&v=beta&t=gxWy-7Qkj5xOcmDi-cO1tXYDd-WP5bhKN71Rf1kA2hE", // URL to Venkata Nagarjun's LinkedIn profile image
-            name: "Venkata Nagarjun Pudureddiyur Manivannan",
+            name: "Venkata Nagarjun",
             headline: "MS Robotics @ Carnegie Mellon University",
             link: "https://www.linkedin.com/in/venkata-nagarjun-pudureddiyur-manivannan-57190b19a/"
         };
@@ -17,31 +17,31 @@ window.addEventListener("DOMContentLoaded", async function () {
         // Creating the LinkedIn card HTML
         el.innerHTML = `
             <div style="font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji; border-radius: 6px; line-height: 1.5; padding: 16px; font-size: 14px; color: #24292e; background-color: #adb8bd;">
-                <div style="display: flex; align-items: center;">
-                    <img style="width: 48px; height: 48px; border-radius: 50%" src="${profile.profile_image}" alt="Profile image"></img>
-                    <div style="display: flex; flex-direction: column; margin-left: 12px">
-                        <span style="font-weight: 500; color: #000000; font-size: 18px">
-                            <a style="text-decoration: none; color: inherit;" target="_blank" href="${profile.link}">
-                                ${profile.name}
-                            </a>
-                        </span>
-                        <span style="font-weight: 400; color: #586069; font-size: 12px">
-                            ${profile.headline}
-                        </span>
-                    </div>
+            <div style="display: flex; align-items: center;">
+                <img style="width: 48px; height: 48px; border-radius: 50%" src="images/profile_pic_edited.jpg" alt="Profile image"></img>
+                <div style="display: flex; flex-direction: column; margin-left: 12px">
+                <span style="font-weight: 500; color: #000000; font-size: 18px">
+                    <a style="text-decoration: none; color: inherit;" target="_blank" href="${profile.link}">
+                    ${profile.name}
+                    </a>
+                </span>
+                <span style="font-weight: 400; color: #586069; font-size: 12px">
+                    ${profile.headline}
+                </span>
                 </div>
-                <div style="margin-top: 12px; display: flex; justify-content: space-evenly; align-items: center; ">
-                    <div style="margin-top: -4px">
-                        <span style="font-size: 10px; font-weight: 500; color: #586069;">
-                            LINKEDIN PROFILE
-                        </span>
-                        <div style="font-weight: 400; color: #211F1F; font-size: 12px; margin-top: 2px">
-                            <a href="${profile.link}" target="_blank" style="font-weight: 600; color: #0077b5; font-size: 14px;">
-                                Visit LinkedIn
-                            </a>
-                        </div>
-                    </div>
+            </div>
+            <div style="margin-top: 12px; display: flex; justify-content: space-evenly; align-items: center; ">
+                <div style="margin-top: -4px">
+                <span style="font-size: 10px; font-weight: 500; color: #586069;">
+                    LINKEDIN PROFILE
+                </span>
+                <div style="font-weight: 400; color: #211F1F; font-size: 12px; margin-top: 2px">
+                    <a href="${profile.link}" target="_blank" style="font-weight: 600; color: #0077b5; font-size: 14px;">
+                    Visit LinkedIn
+                    </a>
                 </div>
+                </div>
+            </div>
             </div>
         `;
     });
